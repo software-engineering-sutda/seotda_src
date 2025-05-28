cd2751c8866831be6edbbec230627db7460d9b58
Butterbean00 <144323817+Butterbean00@users.noreply.github.com>
2025-05-28 20:46:36 +0900

    Update Card.py

--- Card.py
+++ Card.py
@@ -1,18 +1,15 @@
 import random
+from typing import List, Iterator, Callable, Tuple
 
 class Card: 
     def __init__(self, month : int, is_gwang : bool = False, is_yul : bool = False) :
-        self.month: int = month
-        self.is_gwang: bool = is_gwang
-        self.is_yul: bool = is_yul
+        self.month = month
+        self.is_gwang = is_gwang
+        self.is_yul = is_yul
         
     def show(self) -> str:
-        flag = "Normal"
-        if self.is_gwang :
-            flag = "Gwang"
-        elif self.is_yul :
-            flag = "Yul"
-        if flag != "Normal" :
-            return f"{self.month}월{flag}"
-        else :
-            return f"{self.month}월"
+        if self.is_gwang:
+            return f"{self.month}월 Gwang"
+        if self.is_yul:
+            return f"{self.month}월 Yul"
+        return f"{self.month}월"
